41ad5f9b384f4d0ac8a042c7f037a0ea6bc27ab6
dvlang <daniel.v.lang@gmail.com>
2019-06-23 22:24:31 -0500

    cleaned up code, eliminated serial prints

--- Max_Robot_integrated/Max_Robot_integrated.ino
+++ Max_Robot_integrated/Max_Robot_integrated.ino
@@ -1,66 +1,17 @@
 /*
 ================================================================================
 
-    File........... NES Controller Test Code
-    Purpose........ To demonstrate how to interface to an NES controller
-    Author......... Joseph Corleto
-    E-mail......... corleto.joseph @ gmail.com
-    Started........ 04/13/2016
-    Finished....... 04/14/2016
+    File........... 
+    Purpose........ 
+    Author......... 
+    E-mail......... 
+    Started........ 
+    Finished....... 
     Updated........ --/--/----
  
 ================================================================================
    Notes
 ================================================================================
-- The NES controller contains one 8-bit 4021 shift register inside. 
-
-- This register takes parallel inputs and converts them into a serial output.
-
-- This code first latches the data and then shifts in the first bit on the data line. 
-  Then it clocks and shifts in on the data line until all bits are received.
-  
-- What is debugged are the button states of the NES controller.
-
-- A logical "1" means the button is not pressed. A logical "0" means the button is
-  pressed.
-  
-- This code shifts the first bit of data into the LSB.
-
-- The order of shifting for the buttons is shown in the table below:
-
-        Bit# | Button   
-        --------------
-          0  |   A  
-        --------------
-          1  |   B  
-        --------------
-          2  | Select   
-        --------------
-                      3  | Start  
-        --------------
-          4  |   Up  
-        --------------
-          5  |  Down  
-        --------------
-          6  |  Left   
-        --------------
-          7  | Right   
-        --------------
-        
-- The NES controller pinout is shown below (looking into controllers
-  connector end):
-    __________
-   /      |
-  /       O 1 | 1 - Ground
-        |           | 2 - Clock
-  | 7 O   O 2 |   3 - Latch
-  |           | 4 - Data Out
-  | 6 O   O 3 | 5 - No Connection
-  |           |   6 - No Connection
-  | 5 O   O 4 |   7 - +5V
-  |___________|
-
-- Please visit http://www.allaboutcircuits.com to search for complete article!
 
 ================================================================================
   Updates
@@ -160,21 +111,15 @@ const int RIGHT_BUTTON     = 7;
 //===============================================================================
 //  Variables
 //===============================================================================
-byte nesRegister  = 0;    // We will use this to hold current button states
-
-bool apressed=false;
-
-int pwmValue,pwmMax;
 
 //===============================================================================
 //  Pin Declarations
 //===============================================================================
 //Inputs:
-int nesData       = 4;    // The data pin for the NES controller
+
 
 //Outputs:
-int nesClock      = 2;    // The clock pin for the NES controller
-int nesLatch      = 3;    // The latch pin for the NES controller
+
 
 //===============================================================================
 //  Initialization
@@ -182,28 +127,21 @@ int nesLatch      = 3;    // The latch pin for the NES controller
 void setup() 
 {
   // Initialize serial port speed for the serial terminal
-  Serial.begin(9600);
+    Serial.begin(9600);
     XBee.begin(9600);
   
-  // Set appropriate pins to inputs
-  pinMode(nesData, INPUT);
+
+
+    //DEFAULT MOTOR STATES
+    setupArdumoto(); // Set all pins as outputs
+    driveArdumoto(STOP);
   
-  // Set appropriate pins to outputs
-  pinMode(nesClock, OUTPUT);
-  pinMode(nesLatch, OUTPUT);
+    //arm
+    initArm();
   
-  // Set initial states
-  digitalWrite(nesClock, LOW);
-  digitalWrite(nesLatch, LOW);
+    //grip
+    initGrip();
 
-  //DEFAULT MOTOR STATES
-  setupArdumoto(); // Set all pins as outputs
-
-  //arm
-  initArm();
-
-  //grip
-  initGrip();
 }
 
 //===============================================================================
@@ -211,173 +149,85 @@ void setup()
 //===============================================================================
 void loop() 
 {
-  // This function call will return the states of all NES controller's register
-  // in a nice 8 bit variable format. Remember to refer to the table and
-  // constants above for which button maps where!
-  nesRegister = readNesController();
+
   
   // Slight delay before we debug what was pressed so we don't spam the
   // serial monitor.
-  delay(180);
+  delay(1);
   
-  // To give you an idea on how to use this data to control things for your
-  // next project, look through the serial terminal code below. Basically,
-  // just choose a bit to look at and decide what to do whether HIGH (not pushed)
-  // or LOW (pushed). What is nice about this test code is that we mapped all
-  // of the bits to the actual button name so no useless memorizing!
+ // handleSerial();
 
-  /*  
-  if (bitRead(nesRegister, A_BUTTON) == 0){
-        //Serial.println("ARM AND GRIP ENABLE");
-          apressed = true;
-  }
-  else if(bitRead(nesRegister, A_BUTTON) == 1){
-    apressed=false;
-  }
-  else{}
+ //while (Serial.available() > 0) {
+  while (XBee.available() > 0) {
+     //char incomingCharacter = Serial.read();
+     char incomingCharacter = XBee.read();
+     //Serial.println(incomingCharacter); 
 
+     switch (incomingCharacter) {
+     
+       
+       case 'f':
+            //Serial.println("commanded to move forwards"); 
+            driveArdumoto(FORWARD); 
+            break;
+   
+       case 'b':
+            driveArdumoto(BACKWARD);
+            //Serial.println("commanded to move backwards"); 
+            break;
+  
+        case 'r':
+            driveArdumoto(RIGHT);
+            //Serial.println("commanded to move right"); 
+            break;
+  
+        case 'l':
+            driveArdumoto(LEFT);
+            //Serial.println("commanded to move left"); 
+            break;
+  
+        case 'o':
+            adjustGripper(OPEN);
+            //Serial.println("commanded to move open"); 
+            break;
+  
+         case 'c':
+            adjustGripper(CLOSE);
+            //Serial.println("commanded to move close"); 
+            break;
+  
+         case 'u':
+            adjustArm(UP);
+            //Serial.println("commanded to move up"); 
+            break;
+  
+         case 'd':
+            adjustArm(DOWN);
+            //Serial.println("commanded to move down"); 
+            break;
+  
+         case 's':
+            driveArdumoto(STOP);
+            //Serial.println("commanded to move stop"); 
+            break;
     
-  if (bitRead(nesRegister, B_BUTTON) == 0){
-        Serial.println("NOT DEFINED");
-  }
-
-    
-  if (bitRead(nesRegister, START_BUTTON) == 0){
-        Serial.println("LIGHTS ON");
+         default:
+            driveArdumoto(STOP);
+            break;
+  
+      }
   }
 
   
-  if (bitRead(nesRegister, SELECT_BUTTON) == 0){
-        Serial.println("BEEP BEEP");
-  }
-*/
- handleSerial();
-
-
-
-    driveArdumoto(STOP); 
-  //move forward if B button isn't pressed  
-  if (bitRead(nesRegister, UP_BUTTON) == 0){
- //      if(bitRead(nesRegister,A_BUTTON==0)){
-       if(apressed){
-          Serial.println("ARM UP");   
-          adjustArm(UP);
-       }
-       else{
-          Serial.println("MOVE FORWARD");
-          driveArdumoto(FORWARD); 
-       }
-   }
-    
-    //move  backwards if B button isn't pressed
-  if (bitRead(nesRegister, DOWN_BUTTON) == 0){
-       if(apressed){
-          Serial.println("ARM DOWN");   
-          adjustArm(DOWN);
-       }
-       else{
-          Serial.println("MOVE BACKWARDS");
-          driveArdumoto(BACKWARD); 
-       }
-  }
-
-    
-  if (bitRead(nesRegister, LEFT_BUTTON) == 0){
-    if(apressed){
-      Serial.println("OPEN GRIP");  
-      adjustGripper(OPEN);
-    }
-    else  {
-      Serial.println("TURN LEFT");  
-      driveArdumoto(LEFT);
-    }
-  }
   
-  if (bitRead(nesRegister, RIGHT_BUTTON) == 0){
-    if(apressed){
-       Serial.println("CLOSE GRIP");  
-       adjustGripper(CLOSE);
-    }
-    else  {
-      Serial.println("TURN RIGHT ");
-      driveArdumoto(RIGHT); 
-    }
-  }
+  
 }
 
 //===============================================================================
 //  Functions
 //===============================================================================
-///////////////////////
-// readNesController //
-///////////////////////
-byte readNesController() 
-{  
-  // Pre-load a variable with all 1's which assumes all buttons are not
-  // pressed. But while we cycle through the bits, if we detect a LOW, which is
-  // a 0, we clear that bit. In the end, we find all the buttons states at once.
-  int tempData = 255;
-    
-  // Quickly pulse the nesLatch pin so that the register grab what it see on
-  // its parallel data pins.
-  digitalWrite(nesLatch, HIGH);
-  digitalWrite(nesLatch, LOW);
- 
-  // Upon latching, the first bit is available to look at, which is the state
-  // of the A button. We see if it is low, and if it is, we clear out variable's
-  // first bit to indicate this is so.
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, A_BUTTON);
-    
-  // Clock the next bit which is the B button and determine its state just like
-  // we did above.
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, B_BUTTON);
-  
-  // Now do this for the rest of them!
-  
-  // Select button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, SELECT_BUTTON);
 
-  // Start button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, START_BUTTON);
 
-  // Up button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, UP_BUTTON);
-    
-  // Down button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, DOWN_BUTTON);
-
-  // Left button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, LEFT_BUTTON);  
-    
-  // Right button
-  digitalWrite(nesClock, HIGH);
-  digitalWrite(nesClock, LOW);
-  if (digitalRead(nesData) == LOW)
-    bitClear(tempData, RIGHT_BUTTON);
-    
-  // After all of this, we now have our variable all bundled up
-  // with all of the NES button states.*/
-  return tempData;
-}
 
 //MOTOR DRIVER FUNCTIONS
 // driveArdumoto drives 'motor' in 'dir' direction at 'spd' speed
@@ -385,7 +235,7 @@ void driveArdumoto(motionstate waytogo)
 {
   if (waytogo == FORWARD)
   {
-    Serial.println("FORWARD");
+    //Serial.println("FORWARD");
     digitalWrite(LEFTDIR, REV);
     digitalWrite(RIGHTDIR, FWD);
     analogWrite(LEFTPWM, SPD);
@@ -393,7 +243,7 @@ void driveArdumoto(motionstate waytogo)
   }
   else if (waytogo == BACKWARD)
   {
-    Serial.println("BACKWARD");
+  //  Serial.println("BACKWARD");
     digitalWrite(LEFTDIR, FWD);
     digitalWrite(RIGHTDIR, REV);
     analogWrite(LEFTPWM, SPD);
@@ -401,7 +251,7 @@ void driveArdumoto(motionstate waytogo)
   }  
     else if (waytogo == LEFT)
   {
-    Serial.println("LEFT");
+    //Serial.println("LEFT");
     digitalWrite(LEFTDIR, REV);
     digitalWrite(RIGHTDIR, REV);
     analogWrite(LEFTPWM, SPD);
@@ -409,7 +259,7 @@ void driveArdumoto(motionstate waytogo)
   }  
     else if (waytogo == RIGHT)
   {
-    Serial.println("RIGHT");
+  //  Serial.println("RIGHT");
     digitalWrite(LEFTDIR, FWD);
     digitalWrite(RIGHTDIR, FWD);
     analogWrite(LEFTPWM, SPD);
@@ -452,13 +302,13 @@ void setupArdumoto()
 // adjustGripper 
 void adjustGripper(gripmotion dirtomove)
 {
-
+ // Serial.println("called adjust grip"); 
   if(dirtomove==OPEN){
      if (grippos <=180)
      {
         grippos+=5;
         servo2.write(grippos);
-        Serial.println(grippos);   
+       // Serial.println(grippos);   
      }
   }
   else if (dirtomove==CLOSE){
@@ -466,7 +316,7 @@ void adjustGripper(gripmotion dirtomove)
      {
         grippos-=5;
         servo2.write(grippos);
-        Serial.println(grippos);   
+       // Serial.println(grippos);   
      }
   }
   
@@ -475,13 +325,13 @@ void adjustGripper(gripmotion dirtomove)
 // adjustArm 
 void adjustArm(armmotion dirtomove)
 {
-
+ // Serial.println("called adjust arm"); 
   if(dirtomove==UP){
      if (armpos <=85)
      {
         armpos+=5;
         servo1.write(armpos);
-        Serial.println(armpos);   
+       // Serial.println(armpos);   
      }
   }
   else if (dirtomove==DOWN){
@@ -489,7 +339,7 @@ void adjustArm(armmotion dirtomove)
      {
         armpos-=5;
         servo1.write(armpos);
-        Serial.println(armpos);  
+        //Serial.println(armpos);  
      }
   }
   
@@ -513,87 +363,65 @@ void initArm(void)
     servo1.write(armpos);
 }
 
-void handleSerial() {
- //while (Serial.available() > 0) {
-   while (XBee.available() > 0) {
-   //char incomingCharacter = Serial.read();
-   char incomingCharacter = XBee.read();
- //  int pwmValue,pwmMax;
-   switch (incomingCharacter) {
-     case 'f':
-      pwmValue = pwmValue + 5;
-      Serial.println("commanded to move forwards"); 
-      driveArdumoto(FORWARD); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
- 
-     case 'b':
-      pwmValue = pwmValue - 5;
-      driveArdumoto(BACKWARD);
-      Serial.println("commanded to move backwards"); 
-      if (pwmValue <= 0)
-         pwmValue = 0;
-      break;
+/*void handleSerial() {
+  //while (Serial.available() > 0) {
+  while (XBee.available() > 0) {
+     //char incomingCharacter = Serial.read();
+     char incomingCharacter = XBee.read();
+     //Serial.println(incomingCharacter); 
 
-      case 'r':
-      pwmValue = pwmValue + 5;
-      driveArdumoto(RIGHT);
-      Serial.println("commanded to move right"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-       case 'l':
-      pwmValue = pwmValue + 5;
-       driveArdumoto(LEFT);
-      Serial.println("commanded to move left"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-      case 'o':
-      pwmValue = pwmValue + 5;
-      adjustGripper(OPEN);
-      Serial.println("commanded to move open"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-       case 'c':
-      pwmValue = pwmValue + 5;
-      adjustGripper(CLOSE);
-      Serial.println("commanded to move close"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-       case 'u':
-      pwmValue = pwmValue + 5;
-      adjustArm(UP);
-      Serial.println("commanded to move up"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-       case 'd':
-      pwmValue = pwmValue + 5;
-      adjustArm(DOWN);
-      Serial.println("commanded to move down"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-       case 's':
-      pwmValue = pwmValue + 5;
-      driveArdumoto(STOP);
-      Serial.println("commanded to move stop"); 
-      if(pwmValue >= pwmMax)
-         pwmValue = pwmMax;
-      break;
-
-
-      
-    }
- }
-}
+     switch (incomingCharacter) {
+     
+       
+       case 'f':
+            //Serial.println("commanded to move forwards"); 
+            driveArdumoto(FORWARD); 
+            break;
+   
+       case 'b':
+            driveArdumoto(BACKWARD);
+            //Serial.println("commanded to move backwards"); 
+            break;
+  
+        case 'r':
+            driveArdumoto(RIGHT);
+            //Serial.println("commanded to move right"); 
+            break;
+  
+        case 'l':
+            driveArdumoto(LEFT);
+            //Serial.println("commanded to move left"); 
+            break;
+  
+        case 'o':
+            adjustGripper(OPEN);
+            //Serial.println("commanded to move open"); 
+            break;
+  
+         case 'c':
+            adjustGripper(CLOSE);
+            //Serial.println("commanded to move close"); 
+            break;
+  
+         case 'u':
+            adjustArm(UP);
+            //Serial.println("commanded to move up"); 
+            break;
+  
+         case 'd':
+            adjustArm(DOWN);
+            //Serial.println("commanded to move down"); 
+            break;
+  
+         case 's':
+            driveArdumoto(STOP);
+            //Serial.println("commanded to move stop"); 
+            break;
+    
+         default:
+            driveArdumoto(STOP);
+            break;
+  
+      }
+  }
+}*/
